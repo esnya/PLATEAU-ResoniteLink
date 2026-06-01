1aeacbc027306a3c70b04fa2b0e4dccd3ab80be4
esnya <2088693+esnya@users.noreply.github.com>
2026-06-02 03:21:55 +0900

    Fix scene import plan review build

--- src/PlateauResoniteLink/Application/Importing/SceneImportExecutionPlan.cs
+++ src/PlateauResoniteLink/Application/Importing/SceneImportExecutionPlan.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using PlateauResoniteLink.Domain.Importing;
+
 namespace PlateauResoniteLink.Application.Importing;
 
 public sealed record SceneImportExecutionPlan
