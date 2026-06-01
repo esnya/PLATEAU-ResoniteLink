84c4fd8a588ff048389e09155b38c3f7153f44f9
esnya <2088693+esnya@users.noreply.github.com>
2026-06-02 04:44:47 +0900

    Enforce validated import request payloads

--- src/PlateauResoniteLink/Application/Importing/ValidatedDatasetLocation.cs
+++ src/PlateauResoniteLink/Application/Importing/ValidatedDatasetLocation.cs
@@ -16,14 +16,30 @@ public abstract record ValidatedDatasetLocation
     public abstract DatasetLocation ToDatasetLocation();
 }
 
-public sealed record ValidatedLocalDatasetLocation(string LocalSourcePath)
-    : ValidatedDatasetLocation(DatasetSourceKind.Local)
+public sealed record ValidatedLocalDatasetLocation : ValidatedDatasetLocation
 {
+    public ValidatedLocalDatasetLocation(string localSourcePath)
+        : base(DatasetSourceKind.Local)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(localSourcePath);
+        LocalSourcePath = localSourcePath;
+    }
+
+    public string LocalSourcePath { get; }
+
     public override DatasetLocation ToDatasetLocation() => new LocalDatasetLocation(LocalSourcePath);
 }
 
-public sealed record ValidatedRemoteDatasetLocation(Uri ServerUri)
-    : ValidatedDatasetLocation(DatasetSourceKind.Remote)
+public sealed record ValidatedRemoteDatasetLocation : ValidatedDatasetLocation
 {
+    public ValidatedRemoteDatasetLocation(Uri serverUri)
+        : base(DatasetSourceKind.Remote)
+    {
+        ArgumentNullException.ThrowIfNull(serverUri);
+        ServerUri = serverUri;
+    }
+
+    public Uri ServerUri { get; }
+
     public override DatasetLocation ToDatasetLocation() => new RemoteDatasetLocation(ServerUri);
 }
