29b65a6d86750283d8aaa7dc0cef952373f0cc5c
esnya <2088693+esnya@users.noreply.github.com>
2026-06-08 08:13:37 +0900

    Trim live send run starter interface

--- tests/PlateauResoniteLink.Tests/Targets/ResoniteLiveSceneImportTargetTestSupport.cs
+++ tests/PlateauResoniteLink.Tests/Targets/ResoniteLiveSceneImportTargetTestSupport.cs
@@ -322,7 +322,7 @@ internal static class ResoniteLiveSceneImportTargetTestSupport
     public static ResoniteLiveSceneImportDependencies CreateDependencies(
         ILiveSendClientSession session,
         ResoniteLinkSendDiagnostics diagnostics,
-        IResoniteLiveSendRunStarter runStarter)
+        ResoniteLiveSendRunStarter runStarter)
     {
         return new ResoniteLiveSceneImportDependencies(
             session,
