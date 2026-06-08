729a1ae1857c3c20fe1fe912372e210faa912f67
esnya <2088693+esnya@users.noreply.github.com>
2026-06-08 13:58:20 +0900

    Trim queued city object sender

--- tests/PlateauResoniteLink.Tests/Targets/ResoniteLiveSceneImportTargetTestSupport.cs
+++ tests/PlateauResoniteLink.Tests/Targets/ResoniteLiveSceneImportTargetTestSupport.cs
@@ -350,10 +350,9 @@ internal static class ResoniteLiveSceneImportTargetTestSupport
     {
         TerrainTextureAssetGenerator defaultGenerator = new();
         return new ResoniteQueuedCityObjectWorker(
-            new ResoniteQueuedCityObjectSender(
-                new ResoniteQueuedCityObjectPreparation(
-                    generateTerrainTexture ?? defaultGenerator.EnsureTextureAsync),
-                CreatePreparedCityObjectImporter(materialPlanning)));
+            new ResoniteQueuedCityObjectPreparation(
+                generateTerrainTexture ?? defaultGenerator.EnsureTextureAsync),
+            CreatePreparedCityObjectImporter(materialPlanning));
     }
 
     private static ResonitePreparedCityObjectImporter CreatePreparedCityObjectImporter(
